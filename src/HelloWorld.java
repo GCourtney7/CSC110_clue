518b73f9ec0d1cc7004cfeba4cceb1d90b32680c
GCourtney27 <garrett27302@hotmail.com>
2017-11-29 13:16:33 -0700

    Adding a second and third print line

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,10 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
+		System.out.println("Mic Drop");
+		
+		System.out.println("Oops tripped over the mic");
 	}
 
 }
